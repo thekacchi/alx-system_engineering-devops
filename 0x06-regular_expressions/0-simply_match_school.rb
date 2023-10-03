d0e300a73e1167ace8e7308ca54aeb4e03dd26ed
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-03 15:10:02 -0700

    fixed bugs

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -7,10 +7,12 @@ input_string = ARGV[0]
 regex = /School/
 
 # Check if the input_string matches regular expression
-if match_data = input_string.match(regex)
+#if match_data = input_string.scan(regex)
 	#if theres a match, print matched part
-	puts match_data[0]
-else
+#	puts match_data[0]
+#else
 	#print empty string
-	puts ' '
-end
+#	puts ' '
+#end
+matches = input_string.scan(regex)
+puts matches.join
