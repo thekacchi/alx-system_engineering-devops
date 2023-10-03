9519364c5b8dc95890a4a08380e092dfd66d167f
Onyedikachi Eze <thekacchi1@gmail.com>
2023-10-03 15:47:33 -0700

    5, 6, 7

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+#get argument passed to script
+input_string = ARGV[0]
+
+# Define regular expression to match "School"
+regex = /[A-Z]+/
+
+# Use scan method to find all the matches in the input string
+matches = input_string.scan(regex)
+
+# Print matched parts concatenated together
+puts matches.join
