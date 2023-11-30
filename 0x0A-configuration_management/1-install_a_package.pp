90b425625a2425f1d27b06c77f71008ae6db9d55
Onyedikachi Eze <thekacchi1@gmail.com>
2023-11-29 17:09:10 -0800

    thats it

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,9 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+exec { 'echo_flask_version':
+  command => '/bin/echo "flask --version" | /usr/bin/python3',
+  path    => ['/usr/bin', '/bin'],
+  require => Package['Flask'],
+}
