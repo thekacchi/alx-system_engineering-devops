2272ff7991ee1ca11393caa6681e93ba083060cb
Onyedikachi Eze <thekacchi1@gmail.com>
2023-12-04 18:58:55 -0800

    sleepynow

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# Install Nginx
+class { 'nginx':
+  ensure => 'installed',
+}
+
+# Get the hostname
+$hostname = $facts['networking']['hostname']
+
+# Create a custom HTTP response header
+file { '/etc/nginx/sites-available/custom_header':
+  ensure  => 'file',
+  content => "add_header X-Served-By ${hostname};",
+}
+
+# Symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/custom_header':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/custom_header',
+}
+
+# Restart Nginx for changes to take effect
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/custom_header'],
+}
