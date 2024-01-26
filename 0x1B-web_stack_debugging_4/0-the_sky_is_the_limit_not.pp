907380f08285a6c0d7d41b3f05aa0f755922f314
Onyedikachi Eze <thekacchi1@gmail.com>
2024-01-26 14:25:53 -0800

    lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # change the nginx file limit
 exec { 'Change nginx limit':
-  command => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudoservice nginx restart',
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudoservice nginx restart',
   provider => shell,
 }
