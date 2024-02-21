7d6c15dc4e00d9951d0043b987618b6280127efc
thekacchi <thekacchi1@gmail.com>
2024-02-20 17:09:18 -0800

    drank

--- 0x15-api/0-main.py
+++ 0x15-api/0-main.py
@@ -25,6 +25,7 @@ def first_line(id):
     with open(filename, 'r') as f:
         first = f.readline().strip()
 
+    print(first)
     if name in first:
         print(name)
         print("Employee Name: OK")
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,53 @@
+#!/usr/bin/python3
+"""
+Checks student output for returning info from REST API and exports to CSV
+"""
+
 import requests
 import sys
 import csv
 
-def fetch_and_export_to_csv(employee_id):
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(url)
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    if response.status_code == 200:
-        todos = response.json()
-        employee_name = todos[0]['title'].split(" ")[0].title() # Extracting the employee name differently
 
-        csv_filename = f'{employee_id}.csv'
+def fetch_user_name(user_id):
+    """ Fetch user name """
+    resp = requests.get(users_url).json()
+    name = None
+    for i in resp:
+        if i['id'] == user_id:
+            name = i['name']
+    return name
 
-        with open(csv_filename, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-            for task in todos:
-                task_title = task['title']
-                completed_status = str(task['completed'])
-                csv_writer.writerow([employee_id, employee_name, completed_status, task_title])
+def export_to_csv(user_id, tasks):
+    """ Export data to CSV """
+    user_name = fetch_user_name(user_id)
+    filename = f'{user_id}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for task in tasks:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user_name,
+                'TASK_COMPLETED_STATUS': str(task['completed']),
+                'TASK_TITLE': task['title']
+            })
 
-    else:
-        print(f"Failed to fetch data. Status code: {response.status_code}")
+
+def fetch_and_export(user_id):
+    """ Fetch tasks and export to CSV """
+    resp = requests.get(f'{todos_url}?userId={user_id}').json()
+    export_to_csv(user_id, resp)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>")
     else:
         employee_id = int(sys.argv[1])
-        fetch_and_export_to_csv(employee_id)
+        fetch_and_export(employee_id)
