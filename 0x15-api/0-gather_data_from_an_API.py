c1001f2ff8706dc481eb870cbaa3323bf15a583e
thekacchi <thekacchi1@gmail.com>
2024-02-20 16:33:12 -0800

    abeg na

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,18 +16,20 @@ def fetch_todo_list(employee_id):
             'title'
         ] for task in todos if task['completed']]
         total_tasks = len(todos)
-        user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}').json()
+        user_info = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+        ).json()
         employee_name = user_info['name']
         """employee_name = todos[0][
             'title'
         ].split(" ")[1]  # Extracting the employee name differently"""
 
         print(
-              f'Employee {employee_name} is done with tasks '
-              f'{len(completed_tasks)}/{total_tasks}: '
+              f'Employee {employee_name} is done with tasks'
+              f'({len(completed_tasks)}/{total_tasks}):'
         )
         for task_title in completed_tasks:
-            print(f'\t{task_title}')
+            print(f'T {task_title}')
 
     else:
         print(f"Failed to fetch data. Status code: {response.status_code}")
